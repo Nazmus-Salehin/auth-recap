695ad567403d3a92ebddd42da661e08408699d1f
Nazmus-Salehin <16101086@uap-bd.edu>
2021-03-18 06:53:40 +0600

    auth recap

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwW-ui7I_5B3pNSJi8SbEots0NqDSZXuk",
+    authDomain: "auth-recap-44bfc.firebaseapp.com",
+    projectId: "auth-recap-44bfc",
+    storageBucket: "auth-recap-44bfc.appspot.com",
+    messagingSenderId: "452504385605",
+    appId: "1:452504385605:web:166917ea4b9509c6328b2c"
+  };
+  export default firebaseConfig;
